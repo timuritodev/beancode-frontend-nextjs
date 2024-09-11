e329b7f24d92814709de5682a221e78f9d054df5
luciD <sosalnet@yandex.ru>
2024-09-12 02:53:08 +0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'beancode'],
+      domains: ['beancode.ru'], 
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'beancode.ru',
+          pathname: '/api/images/**', 
+        },
+      ],
     },
-};
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
+  
