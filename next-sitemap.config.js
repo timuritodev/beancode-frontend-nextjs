2a5d4cef321d5334de8d4a6075829cabe3c9331e
luciD <sosalnet@yandex.ru>
2024-09-12 03:25:22 +0300

    added robots and sitemap files

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://beancode.ru',  // Укажите URL вашего сайта
+    generateRobotsTxt: true,  // Опция для генерации robots.txt
+    // exclude: ['/admin'],  // Исключенные страницы (настройте под свои нужды)
+  };
+  
+  module.exports = config;
+  
