ffb272fe9f1f594eac7d19d2a639243c5dbc1b53
luciD <sosalnet@yandex.ru>
2025-10-24 20:32:05 +0300

    update deploy

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,14 +1,22 @@
 module.exports = {
-    apps: [
-      {
-        name: 'beancode-frontend-nextjs',
-        script: 'node_modules/.bin/next',
-        args: 'start', // Указывает на команду для запуска в продакшене
-        env: {
-          NODE_ENV: 'production',
-          PORT: 3000,
-        },
-      },
-    ],
-  };
-  
+	apps: [
+		{
+			name: 'beancode-frontend-nextjs',
+			script: 'npm',
+			args: 'start',
+			cwd: '/home/beancode/beancode-frontend-nextjs',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 3000,
+			},
+			instances: 1,
+			exec_mode: 'fork',
+			watch: false,
+			max_memory_restart: '1G',
+			error_file: './logs/err.log',
+			out_file: './logs/out.log',
+			log_file: './logs/combined.log',
+			time: true,
+		},
+	],
+};
